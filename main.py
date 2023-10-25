04f5ffaa41159bb81ccd1acd50744fb04a6df65c
davidalejandroo28 <149003599+davidalejandroo28@users.noreply.github.com>
2023-10-25 16:30:24 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+def menu():
+    print("Menu", "\n------------- \n1. Encode \n2. Decode"
+        "\n3. Quit")
+
+
+
+def decode(password):
+    decode_list =[]
+    for digit in password:
+        decode_list.append(int(digit))
+
+    ranng = len(decode_list)
+
+    for i in range(0,ranng):
+        decode_list[i] = decode_list[i] - 3
+
+    for i in range(0,ranng):
+        decode_list[i] = str(decode_list[i])
+
+    decoded_pass = "".join(decode_list)
+    print(decoded_pass)
+
+
+
+menu()
+#option = int(input("Please enter an option")
+passwrd = input("Please enter your password to encode:")
+
+decode(passwrd)
+
+
